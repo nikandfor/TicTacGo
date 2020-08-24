1c365e7a36f6dae24623d2afb6142622a85e4a0b
Nikifor Seryakov <nikandfor@gmail.com>
2020-08-24 12:44:26 +0300

    Update main.go
    You should not accept this pull request. It's just to show you comments in place.
    
    General advices:
    * Read https://golang.org/doc/effective_go.html. twice.
    * Read more good code: stdlib, google, facebook, uber, any 3rd-party code you use...
    * Use gofmt
    * Use linters: golangci-lint is a good choise.
    * Practice

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"TicTacGo/internal/board"
-	"fmt"
+	"fmt" // first stdlib imports
+
+	// after empty line 3rd-party imports
+
+	"github.com/Kwbmm/TicTacGo/internal/board" // after empty line local module imports
 )
 
 func main() {
@@ -10,7 +13,8 @@ func main() {
 
 	currentPlayer := 'O'
 	nextPlayer := 'X'
-	board := board.Board{BoardState: [9]board.Cell{
+
+	board := board.Board{BoardState: [9]board.Cell{ // too complex struct for simple task
 		board.Cell{Value: '1'},
 		board.Cell{Value: '2'},
 		board.Cell{Value: '3'},
@@ -24,30 +28,36 @@ func main() {
 
 	for {
 		board.PrintBoard()
+
 		fmt.Printf("Player '%c' turn\nChoose a cell from 1 to 9\n\n", currentPlayer)
+
+		// add empty lines to separate logical parts and make code more readable
+
 		choice := 0
 		fmt.Scanf("%d", &choice)
+
 		if choice < 1 || choice > 9 {
 			fmt.Println("Invalid choice!")
 			continue
 		}
+
+		// always consider flat code over nested
+
 		if board.SetCellValue(choice-1, currentPlayer) {
-			if board.HasWinner(choice-1, currentPlayer) {
-				fmt.Printf("Game won by %c\n", currentPlayer)
-				return
-			}
-			if !board.HasEmptyCells() {
-				fmt.Println("Game is draw")
-				return
-			}
-			tmp := currentPlayer
-			currentPlayer = nextPlayer
-			nextPlayer = tmp
-		} else {
 			fmt.Println("Invalid move!")
 			continue
 		}
 
-	}
+		if board.HasWinner(choice-1, currentPlayer) {
+			fmt.Printf("Game won by %c\n", currentPlayer)
+			return
+		}
 
+		if !board.HasEmptyCells() {
+			fmt.Println("Game is draw")
+			return
+		}
+
+		currentPlayer, nextPlayer = nextPlayer, currentPlayer // there is multiassignments in go
+	}
 }
